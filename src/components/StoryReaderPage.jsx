fdf4a6c90e67df03b6761e3ff35871e88c06759b
k132h243 <k132h243@naver.com>
2025-03-14 23:56:09 +0900

    인덱스 > 키워드, 설명수정

--- src/components/StoryReaderPage.jsx
+++ src/components/StoryReaderPage.jsx
@@ -116,8 +116,8 @@ const StoryReaderPage = () => {
       
       <Helmet>
         <meta name="google-site-verification" content="Sx2a79nNCfoTBZTrBoUBsDlFjGlRQoA2u_AqN2QSreI" />
-        <title>LimBooks - 림버스 스토리 리더</title>
-        <meta name="description" content="LimBooks에서 간단히 림버스를." />
+        <title>LimBooks - 림버스 스토리 아카이브</title>
+        <meta name="description" content="LimBooks에서 림버스컴퍼니의 스토리를." />
         <meta name="keywords" content="LimBooks, 림버스, 스토리리더" />
         
         {/* Open Graph 태그 */}
