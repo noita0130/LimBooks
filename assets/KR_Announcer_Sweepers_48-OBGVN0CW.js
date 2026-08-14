324ff8e99aac0ad0b7bcdee62caee512359791db
noita0130 <k132h243@naver.com>
2026-08-15 08:50:43 +0900

    Updates

---
+++ assets/KR_Announcer_Sweepers_48-OBGVN0CW.js
@@ -0,0 +1,23 @@
+const n=[{id:"announcer_equip_48_1",desc:"아나운서 장착",dlg:`청소! 청소를 시작할 시간입니다!
+요즘 들어 홍원의 골목이 어수선하다지만, 이웃분들과 함께라면 헤쳐나갈 수 있을 겁니다.
+오, 오늘 청소도 무사히 끝났으면 좋겠습니다…`},{id:"announcer_cheer_48_1",desc:"시작 대사 1",dlg:`가족에게 들은 소문입니다만, 홍원에서 또 상잔의 비극이 되풀이될 것이라 합니다.
+안타까운 일이지만, 우리에겐 기회일 겁니다! 식사할 장소가 늘어날 겁니다!`},{id:"announcer_cheer_48_2",desc:"시작 대사 2",dlg:`요즘 이웃분의 아이들은 어떻습니까?
+걱정이 많습니다… 집까지 찾아오는 불청객들 때문에 매번 문단속을 시키는데도… 자꾸만 깜빡합니다.
+아이들을 가르치는 것은 언제나 힘든 법입니다.`},{id:"announcer_specialcheer_48_1",desc:"특수 대사 1",dlg:"가족들의 식사를 누군가 방해하고 있습니다…"},{id:"announcer_specialcheer_48_2",desc:"특수 대사 2",dlg:`병목! 병목! 길이 막히고 있습니다!
+여기서 지체했다간… 청소가 미흡해질 겁니다. 이웃 여러분 힘을 내셔야합니다!`},{id:"announcer_enemy_break_48_1",desc:"적 흐트러짐",dlg:"만찬이 차려졌습니다. 굶주린 이웃은 저자에게로 향하십시오."},{id:"announcer_ally_break_48_1",desc:"아군 흐트러짐",dlg:"추락! 추락! 추락! 이웃들은 조심하십시오! 자칫 잘못하면, 날아갑니다아아아!!!"},{id:"announcer_killenemy_48_1",desc:"적 사망",dlg:"쓰레기 확인! 수거! 처리!"},{id:"announcer_ally_dead_48_1",desc:"아군 사망",dlg:`통탄을 금할 수가 없습니다. 옆에 있던 이웃분의 생명이 다하고 말았습니다.
+수거! 이웃의 남은 연료라도 수거해야 합니다!`},{id:"announcer_enemy_adv_48_1",desc:"적 우세",dlg:`우리는 뒷골목의 허가되지 않은 모든 것을 청소해야 합니다. 가족의 죽음에 슬퍼하되, 돌아보아선 안됩니다.
+전진! 전진! 앞으로 향합니다!`},{id:"announcer_ally_adv_48_1",desc:"아군 우세",dlg:`오랜만입니다… 이런 편안한 식사 시간.
+파도를 견뎌낼 만한 자들도 보이지 않습니다. 오늘 밤은 온전히 우리 청소부들의 시간이 될 것 같군요. `},{id:"announcer_ally_advex_48_1",desc:"아군 우세 특수",dlg:`들립니다! 이웃의 소리가 선명히 들립니다!
+저들은 지쳤습니다. 하지만 우리는 이웃과 가족의 연료를 다시 수거해 멀쩡합니다.
+다시 앞으로 갈 기회입니다.`},{id:"announcer_danger_48_1",desc:"위험상황",dlg:`저, 저희가 오늘은 세번째입니까?
+그렇습니다. 밤의 시간이, 가족과 이웃들의 식사 시간이 끝나가고 있습니다.`},{id:"announcer_enemy_specialskill_48_1",desc:"적 특수 스킬",dlg:`들립니다! 이웃의 소리…?
+이웃분들의 소리가 아닙니다. 앞을 주의하십시오. 흑수일지도 모릅니다. 충격에 대비하십시오.`},{id:"announcer_enemy_specialgimmick_48_1",desc:"적 특수 기믹",dlg:"재적일이 곧 시작됩니다. 이웃분들은 움직이는 건물에 주의하십시오."},{id:"announcer_advatk_physical_48_1",desc:"물리 내성 우세",dlg:"갈고리가 쉽게 들어갑니다. 어렵지 않게 연료를 뽑아낼 수 있을 것 같습니다."},{id:"announcer_disadvatk_physical_48_1",desc:"물리 내성 불리",dlg:"멈추십시오! 멈추십시오! 천장 위에 주거지가 있습니다! 갈고리를 빼내십시오, 이웃!"},{id:"announcer_advatk_attr_48_1",desc:"죄악 내성 우세",dlg:`주거 등록이… 방금 만료됐습니다.
+만료! 그렇다면 청소해야 할 쓰레기입니다!`},{id:"announcer_disadvatk_attr_48_1",desc:"죄악 내성 불리",dlg:"주거 등록 확인. 비, 비켜가겠습니다."},{id:"announcer_enemy_specialbuff_48_1",desc:"적 특수 버프",dlg:`이상! 이상! 길을 막고 있는 사람이 뭔가 이상합니다!
+하지만 멈출 순 없습니다… 조심히… 다가갑시다.`},{id:"announcer_enemy_specialdebuff_48_1",desc:"적 특수 디버프",dlg:"길을 막고 있는 자들은 지쳤습니다! 곧 쓸어버릴 수 있습니다!"},{id:"announcer_ally_specialbuff_48_1",desc:"아군 특수 버프",dlg:"이웃분들이 연료를 가득 채워 오셨군요, 안심이 됩니다."},{id:"announcer_ally_specialdebuff_48_1",desc:"아군 특수 디버프",dlg:`곤란한 곳까지 떨어졌네요.
+괜찮습니다! 가야할 곳은 결국 같습니다! 방향도 같습니다!`},{id:"announcer_enemy_destroy_48_1",desc:"부위 파괴 1",dlg:"허가되지 않은 건축물을 철거했습니다."},{id:"announcer_enemy_destroy_48_2",desc:"부위 파괴 2",dlg:`해체! 붕괴! 액화! 즐거운 식사 시간이었습니다!
+벌써부터 들뜨면 안 됩니다… 아직 골목에 청소해야할 쓰레기가 잔뜩 있습니다.`},{id:"announcer_round_takebigdmg_48_1",desc:"아군 큰 피해 1",dlg:`또입니까!?
+이웃분들이 많이 죽었습니다. 홍원의 밤이 짧지는 않을 텐데… 저들은 어째서…우리의 밤까지 침범하려는 겁니까?`},{id:"announcer_round_takebigdmg_48_2",desc:"아군 큰 피해 2",dlg:`어제도, 오늘도, 내일도… 이곳의 밤은 우리의 밤이었어야 합니다.
+밤중에도 골목마다 여러 세력들이 많이 보입니다. 굶는 가족이 생기기 전에 어머니께 말씀드려야 합니다.`},{id:"announcer_round_givebigdmg_48_1",desc:"적 큰 피해 1",dlg:"만복! 만복의 시간입니다!"},{id:"announcer_round_givebigdmg_48_2",desc:"적 큰 피해 2",dlg:"방향을 헷갈리지 마십시오! 식사할 장소는 이곳입니다! 배불리 연료를 채우고 가야합니다, 이웃!"},{id:"announcer_multikillenemy_48_1",desc:"적 다수 처치",dlg:`성찬을 차려두셨군요. 베품에 감사합니다.
+이, 이렇게 서로 돕고 도와야, 가족들도, 이웃들도, 아이들도 행복합니다.`},{id:"announcer_battle_win_48_1",desc:"전투 승리",dlg:`이웃분들이 다함께 배불리 먹었습니다.
+어머니의 기쁨! 벌써부터 느껴지는 것만 같습니다!`},{id:"announcer_battle_defeat_48_1",desc:"전투 패배",dlg:`퇴근! 퇴근 시간입니다! 기다리고 있는 아이들에게 돌아갈 시간입니다!
+서, 서둘러 돌아갑니다… 괜히 낮에 모습을 드러내는 것은… 골목의 주민들에게도 민폐가 됩니다.`}],e={dataList:n};export{n as dataList,e as default};
--- deployTime.js
+++ deployTime.js
@@ -1,2 +1,2 @@
 // 이 파일은 자동 생성됩니다. 수정하지 마세요.
-export const DEPLOY_TIME = "2026. 03. 08. 13:36:20";
+export const DEPLOY_TIME = "2026. 08. 15. 08:50:39";
